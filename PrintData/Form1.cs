0783485dc2878720c8766ae15ebd814d810bd75f
Михаил Пичугин <michaelpichugin@yandex.ru>
2018-03-29 18:52:53 +0700

    DataGridView Printer`ok C:

---
+++ PrintData/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetupThePrinting();
+            printDocument1.Print();
+        }
+
+        DataGridViewPrinter MyDataGridViewPrinter;
+
+        private bool SetupThePrinting()
+        {
+            PrintDialog MyPrintDialog = new PrintDialog();
+            MyPrintDialog.AllowCurrentPage = false;
+            MyPrintDialog.AllowPrintToFile = false;
+            MyPrintDialog.AllowSelection = false;
+            MyPrintDialog.AllowSomePages = false;
+            MyPrintDialog.PrintToFile = false;
+            MyPrintDialog.ShowHelp = false;
+            MyPrintDialog.ShowNetwork = false;
+
+            if (MyPrintDialog.ShowDialog() != DialogResult.OK)
+                return false;
+
+            printDocument1.DocumentName = this.Text;
+            printDocument1.PrinterSettings = MyPrintDialog.PrinterSettings;
+            printDocument1.DefaultPageSettings = MyPrintDialog.PrinterSettings.DefaultPageSettings;
+            printDocument1.DefaultPageSettings.Landscape = true;
+            printDocument1.DefaultPageSettings.Margins = new Margins(15, 15, 15, 15);
+
+            MyDataGridViewPrinter = new DataGridViewPrinter(dataGridView1,
+            printDocument1, true, true, this.Text + " " + "ЛОЛ 1" + " : " + "ЛОЛ 2", new Font("Tahoma", 8,
+                FontStyle.Bold, GraphicsUnit.Point), Color.Black, true);
+
+            return true;
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            bool more = MyDataGridViewPrinter.DrawDataGridView(e.Graphics);
+            if (more == true)
+                e.HasMorePages = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                dataGridView1.Rows.Add(1, 2,3,4,5,6,7,8,9,10,11,12,13,14);
+            }
+
+            //чётные
+            dataGridView1.RowsDefaultCellStyle.BackColor = Color.Crimson;
+            //нечётные
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.Bisque;
+        }
+    }
+}
